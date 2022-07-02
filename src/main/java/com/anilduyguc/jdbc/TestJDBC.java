91583e3abbc1d4d6139bfab4c9f0b9a273ca1eae
MrKenobii <anldygc35@windowslive.com>
2022-07-02 16:28:51 +0300

    Connecting to the MySQL Server and creating first Student Entity with @Entity @Table annotation

---
+++ src/main/java/com/anilduyguc/jdbc/TestJDBC.java
@@ -0,0 +1,21 @@
+package com.anilduyguc.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+        String jdbcurl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Connecting to database" + jdbcurl);
+            Connection myConn = DriverManager.getConnection(jdbcurl, user, pass);
+            System.out.println("Connection successful");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
