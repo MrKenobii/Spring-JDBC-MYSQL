a6f6e9e071466e27bc1b4de5d46adde0533fce69
MrKenobii <anldygc35@windowslive.com>
2022-07-05 16:08:43 +0300

    Deleting bi directional records

--- src/main/java/com/anilduyguc/uni/DeleteInstructorDetailDemo.java
+++ src/main/java/com/anilduyguc/uni/DeleteInstructorDetailDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class GetInstructorDetailDemo {
+public class DeleteInstructorDetailDemo {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -16,14 +16,17 @@ public class GetInstructorDetailDemo {
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
-            int id = 222;
+            int id = 3;
             InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
 
             System.out.println("Instructor details with id: " + id + " -> " +instructorDetail);
             System.out.println("The associated instructor is: " + instructorDetail.getInstructor());
+            System.out.println("Deleting instructor detail: " + instructorDetail);
+
+            instructorDetail.getInstructor().setInstructorDetail(null); //break bidirectional link, remove the associated object reference
+            session.delete(instructorDetail);
 
             session.getTransaction().commit();
-
             System.out.println("Done!!");
         } catch (Exception e) {
             e.printStackTrace();
